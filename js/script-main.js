b0258f6c5a88edacaa9ab16359e2ba9ad31c5a1c
Antonis <marasantonis@gmail.com>
2021-12-27 14:42:05 +0200

    varibles declared for player values and computer + function tha calulates the resurlt of first round

--- js/script-main.js
+++ js/script-main.js
@@ -7,7 +7,31 @@ function computerPlay() {
   } else if (number === 2) {
     return "Paper";
   } else {
-    return "Scissors";
+    return "Scissor";
   }
 }
-console.log(computerPlay());
+
+let playerSelection = prompt("Rock Paper Scissor");
+playerSelection =
+  playerSelection.charAt(0).toUpperCase() +
+  playerSelection.substring(1).toLowerCase();
+console.log(playerSelection);
+let computerSelection = computerPlay();
+console.log(computerSelection);
+
+// function that calulates the result of one round
+
+function getPlayRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Draw";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You won";
+  } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
+    return "You won";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
+    return "You won";
+  } else {
+    return "You lost";
+  }
+}
+console.log(getPlayRound(playerSelection, computerSelection));
