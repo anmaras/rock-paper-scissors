724ed4e732805af3fe1078f778b5286eee6a3094
Antonis <marasantonis@gmail.com>
2021-12-27 21:45:12 +0200

    playAround function update

--- js/script-main.js
+++ js/script-main.js
@@ -1,41 +1,40 @@
-const playerSelection = userPlay();
+const playerSelection = prompt("Rock Paper Scissor");
 console.log(playerSelection);
 const computerSelection = computerPlay();
 console.log(computerSelection);
-
-//Function that turns the promt imput to lowercase and first letter to uppercase!
-
-function userPlay() {
-  input = prompt("Rock Paper Scissor");
-  return input.charAt(0).toUpperCase() + input.substring(1).toLowerCase();
-}
 
 // computer play function for random number from 1 to 3 with if condition to return to string names
 
 function computerPlay() {
   let number = Math.floor(Math.random() * 3) + 1;
   if (number === 1) {
-    return "Rock";
+    return "rock";
   } else if (number === 2) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissor";
+    return "scissor";
   }
 }
 
-// function that calulates the result of one round
+// // function that calulates the result of one round
 
-function getPlayRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
+function playAround(user, computer) {
+  if (user === computer) {
     return "Draw";
-  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    return `You Won! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
-    return `You Won! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
-    return `You Won! ${playerSelection} beats ${computerSelection}`;
+  } else if (user === "rock" && computer === "scissor") {
+    return `You Won! ${user} beats ${computer}`;
+  } else if (user === "paper" && computer === "rock") {
+    return `You Won! ${user} beats ${computer}`;
+  } else if (user === "scissor" && computer === "paper") {
+    return `You Won! ${user} beats ${computer}`;
+  } else if (user === "scissor" && computer === "rock") {
+    return `You lost ${computer} beats ${user}`;
+  } else if (user === "rock" && computer === "paper") {
+    return `You lost ${computer} beats ${user}`;
+  } else if (user === "paper" && computer === "scissor") {
+    return `You lost ${computer} beats ${user}`;
   } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    return `Thats not right!`;
   }
 }
-console.log(getPlayRound(playerSelection, computerSelection));
+console.log(playAround(playerSelection, computerSelection));
