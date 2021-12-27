2ed2b13965fa4d8cecaaa4653260f1ab3714bc8b
Antonis <marasantonis@gmail.com>
2021-12-27 13:20:35 +0200

    combined random function with if condition

--- js/script-main.js
+++ js/script-main.js
@@ -1,16 +1,13 @@
-// computer play function for random number from 1 to 3
+// computer play function for random number from 1 to 3 with if condition to turn to strings
 
 function computerPlay() {
-  let num = Math.floor(Math.random() * 3) + 1;
-  return num;
+  let number = Math.floor(Math.random() * 3) + 1;
+  if (number === 1) {
+    return "Rock";
+  } else if (number === 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
-// if condition for turning random numbers from computerPlay function to strings
-
-if (computerPlay() === 1) {
-  console.log(computerPlay());
-  console.log("Rock");
-} else if (computerPlay() === 2) {
-  console.log("Paper");
-} else {
-  console.log("Scissors");
-}
+console.log(computerPlay());
