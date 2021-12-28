d9ac2d767f1c2e9200849c0785731b82fdff382a
Antonis <marasantonis@gmail.com>
2021-12-28 18:04:23 +0200

    game function added plus for loop

--- js/script-main.js
+++ js/script-main.js
@@ -1,57 +1,76 @@
-const computerSelection = computerPlay();
-console.log(computerSelection);
-let playerSelection = prompt("Rock Paper Scissor");
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0;
 
-// if condition for avoiding type error with null when using case incensitive prompt!
+game();
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    let playerSelection = prompt("Rock Paper Scissor");
+    let computerSelection = computerPlay();
+    // if condition for avoiding type error with null when using case incensitive prompt!
+    if (playerSelection != null) {
+      playerSelection = playerSelection.toLowerCase();
+    }
+    if (playerSelection === "") {
+      alert("Dude that's empty, play something!   :( ");
+    } else if (playerSelection === null) {
+      alert("Thank you for not playing    -__-");
+      {
+        break;
+      }
+    } else if (
+      playerSelection !== "rock" &&
+      playerSelection !== "paper" &&
+      playerSelection !== "scissor"
+    ) {
+      alert("Please type Rock, Paper or Scissor");
+    }
 
-if (playerSelection != null) {
-  playerSelection = playerSelection.toLowerCase();
-}
-if (playerSelection === "") {
-  alert("Dude that's empty, type something!   :( ");
-} else if (playerSelection === null) {
-  alert("Thank you for not playing    -__-");
-} else if (
-  playerSelection !== "rock" &&
-  playerSelection !== "paper" &&
-  playerSelection !== "scissor"
-) {
-  alert("What the hell did you typed?");
-}
-console.log(playerSelection);
+    // computer play function for random number from 1 to 3 with if condition to return to string names
 
-// computer play function for random number from 1 to 3 with if condition to return to string names
+    function computerPlay() {
+      let number = Math.floor(Math.random() * 3) + 1;
+      if (number === 1) {
+        return "rock";
+      } else if (number === 2) {
+        return "paper";
+      } else {
+        return "scissor";
+      }
+    }
 
-function computerPlay() {
-  let number = Math.floor(Math.random() * 3) + 1;
-  if (number === 1) {
-    return "rock";
-  } else if (number === 2) {
-    return "paper";
-  } else {
-    return "scissor";
+    // // function that calulates the result of one round
+
+    function playAround(user, computer) {
+      if (user === computer) {
+        draw += 1;
+        return alert("It's a draw");
+      } else if (user === "rock" && computer === "scissor") {
+        playerScore += 1;
+        return alert("You Won!" + `${user} wins ${computer}`);
+      } else if (user === "paper" && computer === "rock") {
+        playerScore += 1;
+        return alert("You Won!" + `${user} wins ${computer}`);
+      } else if (user === "scissor" && computer === "paper") {
+        playerScore += 1;
+        return alert("You Won!" + `${user} wins ${computer}`);
+      } else if (user === "scissor" && computer === "rock") {
+        computerScore += 1;
+        return alert(`You lost ${computer} beats ${user}`);
+      } else if (user === "rock" && computer === "paper") {
+        computerScore += 1;
+        return alert(`You lost ${computer} beats ${user}`);
+      } else if (user === "paper" && computer === "scissor") {
+        computerScore += 1;
+        return alert(`You lost ${computer} beats ${user}`);
+      } else {
+        return alert(`Abort`);
+      }
+    }
+    playAround(playerSelection, computerSelection);
+
+    console.log("Player " + playerScore);
+    console.log("Computer " + computerScore);
+    console.log("Draw " + draw);
   }
 }
-
-// // function that calulates the result of one round
-
-function playAround(user, computer) {
-  if (user === computer) {
-    return "draw";
-  } else if (user === "rock" && computer === "scissor") {
-    return "You Won!" + `${user} wins ${computer}`;
-  } else if (user === "paper" && computer === "rock") {
-    return "You Won!" + `${user} wins ${computer}`;
-  } else if (user === "scissor" && computer === "paper") {
-    return "You Won!" + `${user} wins ${computer}`;
-  } else if (user === "scissor" && computer === "rock") {
-    return `You lost ${computer} beats ${user}`;
-  } else if (user === "rock" && computer === "paper") {
-    return `You lost ${computer} beats ${user}`;
-  } else if (user === "paper" && computer === "scissor") {
-    return `You lost ${computer} beats ${user}`;
-  } else {
-    return `Abort`;
-  }
-}
-console.log(playAround(playerSelection, computerSelection));
