849f2875a6a976017f44dcdf1fe2f313920840e1
Antonis <marasantonis@gmail.com>
2021-12-28 16:14:50 +0200

    function updates

--- js/script-main.js
+++ js/script-main.js
@@ -1,48 +1,76 @@
-const computerSelection = computerPlay();
-console.log(computerSelection);
 let playerSelection = prompt("Rock Paper Scissor");
-// if condition for avoiding type error with null when using case incensitive prompt!
-if (playerSelection != null) {
-  playerSelection = playerSelection.toLowerCase();
+
+console.log("player " + playerSelection);
+let computerSelection = computerPlay();
+console.log("Computer " + computerSelection);
+let roundResult = playAround(playerSelection, computerSelection);
+// console.log(roundResult);
+let playerWins = 0;
+let draw = 0;
+let computerWins = 0;
+
+// computer play function for random number from 1 to 3 with if condition to return string names
+
+function computerPlay() {
+  let number = Math.floor(Math.random() * 3) + 1;
+  if (number === 1) {
+    return "rock";
+  } else if (number === 2) {
+    return "paper";
+  } else {
+    return "scissor";
+  }
 }
+
+//Conditions for "" and null choises and wrong words
+
 if (playerSelection === "") {
-  alert("Dude that's empty, type something!   :( ");
+  alert("Yo! that's empty, try to type something!");
 } else if (playerSelection === null) {
   alert("Thank you for not playing    -__-");
+} else if (
+  playerSelection != "rock" &&
+  playerSelection != "paper" &&
+  playerSelection != "scissor"
+) {
+  alert("Thats not the correct words :/");
 }
 
-// computer play function for random number from 1 to 3 with if condition to return to string names
+// function that calulates the result of one round
 
-// function computerPlay() {
-//   let number = Math.floor(Math.random() * 3) + 1;
-//   if (number === 1) {
-//     return "rock";
-//   } else if (number === 2) {
-//     return "paper";
-//   } else {
-//     return "scissor";
-//   }
-// }
-
-// // function that calulates the result of one round
-
-// function playAround(user, computer) {
-//   if (user === computer) {
-//     return draw;
-//   } else if (user === "rock" && computer === "scissor") {
-//     return playerWinRound + `${user} wins ${computer}`;
-//   } else if (user === "paper" && computer === "rock") {
-//     return playerWinRound + `${user} wins ${computer}`;
-//   } else if (user === "scissor" && computer === "paper") {
-//     return playerWinRound + `${user} wins ${computer}`;
-//   } else if (user === "scissor" && computer === "rock") {
-//     return `You lost ${computer} beats ${user}`;
-//   } else if (user === "rock" && computer === "paper") {
-//     return `You lost ${computer} beats ${user}`;
-//   } else if (user === "paper" && computer === "scissor") {
-//     return `You lost ${computer} beats ${user}`;
-//   } else {
-//     return `${user} is an Wrong choice!`;
-//   }
-// }
+function playAround(user, computer) {
+  if (user === computer) {
+    return "Draw";
+  } else if (user === "rock" && computer === "scissor") {
+    return "You won!";
+  } else if (user === "paper" && computer === "rock") {
+    return "You won!";
+  } else if (user === "scissor" && computer === "paper") {
+    return "You won";
+  } else if (user === "scissor" && computer === "rock") {
+    return "You lost";
+  } else if (user === "rock" && computer === "paper") {
+    return "You lost";
+  } else if (user === "paper" && computer === "scissor") {
+    return "You lost";
+  } else {
+    return "That failed!";
+  }
+}
+console.log(playAround(playerSelection, computerPlay()));
 // console.log(playAround(playerSelection, computerPlay()));
+// console.log(playAround(playerSelection, computerPlay()));
+// console.log(playAround(playerSelection, computerPlay()));
+// console.log(playAround(playerSelection, computerPlay()));
+
+function roundScore() {
+  if (roundResult == "Draw") {
+    return (draw += 1);
+  } else if (roundResult === "You won!") {
+    return (playerWins += 1);
+  } else {
+    return (computerWins += 1);
+  }
+}
+roundScore();
+console.log(playerWins, computerWins, draw);
