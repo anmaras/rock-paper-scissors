2301a9a9fa03aac2e2d8a721f7c3d945b3d6fc24
Antonis <marasantonis@gmail.com>
2022-01-13 18:00:34 +0200

    quit btn function added

--- script-main.js
+++ script-main.js
@@ -2,6 +2,7 @@ const toggleStart = document.querySelector(".play-button");
 const startPlayBtn = document.querySelector(".play-button button");
 const gameStart = document.querySelector(".score");
 const gameMainBox = document.querySelector(".ingame-box");
+const quitButton = document.querySelector(".quit_btn button");
 
 const startMainGame = () => {
   toggleStart.classList.toggle("hidden");
@@ -10,3 +11,4 @@ const startMainGame = () => {
 };
 
 startPlayBtn.addEventListener("click", startMainGame);
+quitButton.addEventListener("click", startMainGame);
