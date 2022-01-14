eb7ab7a3902469dc973871dda864104093b5b119
Antonis <marasantonis@gmail.com>
2022-01-14 19:47:23 +0200

    event listener added with user and computer fanction

--- script-main.js
+++ script-main.js
@@ -3,10 +3,7 @@ const startPlayBtn = document.querySelector(".play-button button");
 const gameStart = document.querySelector(".score");
 const gameMainBox = document.querySelector(".ingame-box");
 const quitButton = document.querySelector(".quit_btn");
-const playerButtons = document.querySelector(".player_btns");
-const playerRockSelection = playerButtons.children[0];
-const playerScissorsSelection = playerButtons.children[1];
-const playerPaperSelection = playerButtons.children[2];
+const playerButtons = document.querySelectorAll(".player_btns button");
 
 const startMainGame = () => {
   toggleStart.classList.toggle("hidden");
@@ -15,7 +12,7 @@ const startMainGame = () => {
   quitButton.classList.toggle("visible");
 };
 
-const getComputerChoice = () => {
+const computer = () => {
   let number = Math.floor(Math.random() * 3) + 1;
   if (number === 1) {
     return (number = "Rock");
@@ -24,8 +21,27 @@ const getComputerChoice = () => {
   } else return (number = "Paper");
 };
 
-playerRockSelection.addEventListener("click", getPlayerChoice);
+const playRound = (user, computerChoice) => {
+  if (user === "Rock" && computerChoice === "Scissors") {
+    return console.log("win");
+  } else if (user === "Scissors" && computerChoice === "Paper") {
+    return console.log("win");
+  } else if (user === "Paper" && computerChoice === "Rock") {
+    return console.log("win");
+  } else if (user === computerChoice) {
+    return console.log("Draw");
+  } else return console.log("Lose");
+};
+
+playerButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const user = button.id;
+    console.log(user);
+    const computerChoice = computer();
+    console.log(computerChoice);
+    playRound(user, computerChoice);
+  });
+});
+
 startPlayBtn.addEventListener("click", startMainGame);
 quitButton.addEventListener("click", startMainGame);
-
-console.log(getPlayerChoice());
