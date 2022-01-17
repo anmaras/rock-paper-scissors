d3f8cb7c37e1d90b26a7a65caf1c2b5e8cc2a4b3
Antonis <marasantonis@gmail.com>
2022-01-17 14:02:32 +0200

    added function for round number

--- script-main.js
+++ script-main.js
@@ -4,7 +4,7 @@ const gameStart = document.querySelector(".score");
 const gameMainBox = document.querySelector(".ingame-box");
 const quitButton = document.querySelector(".quit_btn");
 const playerButtons = document.querySelectorAll(".player_btns button");
-const roundNumber = document.querySelector(".game_round span");
+const roundNumberDisplay = document.querySelector(".game_round span");
 const playerWinDisplay = document.querySelector(".player_win");
 const computerWinDisplay = document.querySelector(".computer_win");
 let userScore = 0;
@@ -18,6 +18,7 @@ const startMainGame = () => {
   quitButton.classList.toggle("visible");
 };
 
+//computer choice result
 const calcComputerChoice = () => {
   let number = Math.floor(Math.random() * 3 + 1);
   if (number === 1) {
@@ -27,6 +28,7 @@ const calcComputerChoice = () => {
   } else return (number = "Paper");
 };
 
+//Game round result
 const calcGameRound = (user, computerChoice) => {
   if (user === "Rock" && computerChoice === "Scissors") {
     userScore++;
@@ -43,20 +45,36 @@ const calcGameRound = (user, computerChoice) => {
   computerScore++;
 };
 
+const getRoundNumber = () => {
+  return roundNumberDisplay.textContent++;
+};
+
+//who won the game function
+const checkRoundConditions = () => {
+  if (userScore >= 5) {
+    return alert("You win");
+  } else if (computerScore >= 5) {
+    return alert("You Lost");
+  }
+};
+
 playerButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    roundNumber.textContent++;
     const user = button.id;
-    const computerChoice = calcComputerChoice();
+    computerChoice = calcComputerChoice();
     calcGameRound(user, computerChoice);
     playerWinDisplay.textContent = userScore;
     computerWinDisplay.textContent = computerScore;
     console.log(roundResult);
-    console.log(`Player ${user}`);
+    // console.log(`Player ${user}`);
     console.log(`Computer ${computerChoice}`);
-    console.log(`Score ${userScore} - ${computerScore}`);
+    // console.log(`Score ${userScore} - ${computerScore}`);
   });
 });
+
+// playerButtons.forEach((button) => {
+//   button.addEventListener("click", checkRoundConditions);
+// });
 
 startPlayBtn.addEventListener("click", startMainGame);
 quitButton.addEventListener("click", startMainGame);
